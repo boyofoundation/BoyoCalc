6d49816609634d52250ce0df664af3f340044b36
killkli <dchensterebay@gmail.com>
2022-04-06 15:15:25 +0800

    deploy

---
+++ precache-manifest.9329d8aaa4d4ab75ad10ff3ffae61cd6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e4a1f04e76a5af55c28b60956ca5f6",
+    "url": "/BoyoCalc/.htaccess"
+  },
+  {
+    "revision": "539ac65028c278306d3184013b067147",
+    "url": "/BoyoCalc/.htaccess_AMS.txt"
+  },
+  {
+    "revision": "66e4a1f04e76a5af55c28b60956ca5f6",
+    "url": "/BoyoCalc/.htaccess_DO.txt"
+  },
+  {
+    "revision": "c677f09cc4996ce23fd3",
+    "url": "/BoyoCalc/css/app.a5a462c3.css"
+  },
+  {
+    "revision": "7917b277bf81fde2448b",
+    "url": "/BoyoCalc/css/chunk-vendors.71d29630.css"
+  },
+  {
+    "revision": "bb4acd434607b4a15207b7c02ecc75b6",
+    "url": "/BoyoCalc/fonts/jackeyfont.bb4acd43.ttf"
+  },
+  {
+    "revision": "f530eb5a3854718c0fd75e7a2e394102",
+    "url": "/BoyoCalc/img/Keyboard.f530eb5a.jpg"
+  },
+  {
+    "revision": "0acdbc4d2600e66eee9174393639cfd6",
+    "url": "/BoyoCalc/index.html"
+  },
+  {
+    "revision": "c677f09cc4996ce23fd3",
+    "url": "/BoyoCalc/js/app.b8877c86.js"
+  },
+  {
+    "revision": "7917b277bf81fde2448b",
+    "url": "/BoyoCalc/js/chunk-vendors.18558c08.js"
+  },
+  {
+    "revision": "1bcab96494c78955c93b0787a9482d85",
+    "url": "/BoyoCalc/manifest.json"
+  },
+  {
+    "revision": "89a9dcc447292031329e4eb606bdf963",
+    "url": "/BoyoCalc/media/Button.89a9dcc4.mp3"
+  },
+  {
+    "revision": "b1b2d449f16d132fe8bf2bd9b8bdce4a",
+    "url": "/BoyoCalc/media/GamePlay.b1b2d449.mp3"
+  },
+  {
+    "revision": "d036f8aa13475b045051283c7d5c8eff",
+    "url": "/BoyoCalc/media/keyboard.d036f8aa.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BoyoCalc/robots.txt"
+  }
+]);
